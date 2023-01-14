c0da986d67e17a2f3ca6e5cf111ac65fd33ea935
+ <+>
2023-01-14 07:15:18 +0300

    Completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+function EyesOnMe() {
+  function handleFocus(event) {
+    console.log("Good!");
+    console.log(event.target.value);
+  }
+  function handleBlur(event) {
+    console.log("Hey! Eyes on me!");
+    console.log(event.target.value);
+  }
+  return (
+    <div>
+      <button type="text" onFocus={handleFocus} onBlur={handleBlur}>
+        Eyes on me
+      </button>
+    </div>
+  );
+}
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,16 @@
 // Code Keypad Component Here
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  function handleChange(event) {
+    console.log("Entering password...");
+    console.log(event.target.value);
+  }
+
+  return (
+    <div>
+      <input type="password" onChange={handleChange}></input>
+    </div>
+  );
 }
 
 export default Keypad;
